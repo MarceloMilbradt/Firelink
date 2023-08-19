694ff9b805cfa7d86e6f7d7013386c01292c1ac0
MarceloMilbradt <milbradthz@gmail.com>
2023-08-19 20:32:57 -0300

    Update ConfigureServices.cs

--- Firelink.App/Infrastructure/ConfigureServices.cs
+++ Firelink.App/Infrastructure/ConfigureServices.cs
@@ -13,7 +13,7 @@ public static class ConfigureServices
 
         services.AddSingleton<ISpotifyApi, SpotifyApi>();
         services.AddTransient<IDateTime, DateTimeProvider>();
-        services.AddTransient<IPlayerListenerService, PlayerListenerService>();
+        services.AddSingleton<IPlayerListenerService, PlayerListenerService>();
         services.AddTransient<ISpotifyTrackAnalyticsService, SpotifyTrackAnalyticsService>();
         services.AddTransient<ITuyaConnector,TuyaConnectorService>();
         services.AddLazyCache();
